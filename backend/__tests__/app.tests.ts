81d5c31ab23a6272c8f14d421b94e40b0e79030f
Krzysztof Wdowczyk <krzys.wdowczyk@gmail.com>
2024-01-13 00:44:37 +0100

    Finished version of test 401 GET /recipe

--- backend/__tests__/app.tests.ts
+++ backend/__tests__/app.tests.ts
@@ -2,13 +2,24 @@ import supertest from 'supertest'
 import app from '../src/server'
 import { describe, test, expect } from '@jest/globals';
 
-describe("POST auth/login", () => {
+describe("GET /recipe", () => {
 
-    test("Should respond with a 200 status code", async () => {
-        const response = await supertest(app).post("keepitrealnig").send({
-            'name': 'Bob'}
+    test("Unauthorized - should respond with a 401 status code", async () => {
+        const response = await supertest(app).get("/recipe").query(
+                {
+                page: 1,
+                limit: 10,
+                excludeMyRecipes: false,
+                categoryName: null,
+                ingredientName: null,
+                }
         )
-        expect(response.status).toBe(200);
+
+        console.dir(response.body); 
+        expect(response.status).toBe(401);
+        expect(response.body).toEqual({ success: false, msg: 'Unauthorized' });
     })
 
 })
+
+//  .set('Authorization', "Bearer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
